da82de4673f3fe36ba28d08debaad3648ef95e0e
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-10-27 19:51:25 +0200

    Initialising the User class

---
+++ src/main/java/org/bookhaven/User.java
@@ -0,0 +1,33 @@
+package org.bookhaven;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.tinylog.Logger;
+
+
+@Table(name = "users")
+@Entity
+public class User {
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Setter
+    @Column(unique = true)
+    private String username;
+
+    @Getter
+    @Setter
+    @Column(unique = true)
+    private String email;
+
+    @Getter
+    @Setter
+    private String password;
+
+
+}
