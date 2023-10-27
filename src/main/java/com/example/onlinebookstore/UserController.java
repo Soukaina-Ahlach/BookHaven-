f8bd2beb456850448dc5167cd2ae21268a14bf0e
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-10-27 19:58:18 +0200

    Initialising the UserController class

---
+++ src/main/java/com/example/onlinebookstore/UserController.java
@@ -0,0 +1,46 @@
+package com.example.onlinebookstore;
+
+import jakarta.validation.Valid;
+
+import org.bookhaven.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.tinylog.Logger;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody User user) {
+        Logger.debug("MOVED PAST APPLICATION");
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return ResponseEntity.badRequest().body("Username already exists");
+        }
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    // Other methods
+
+}
