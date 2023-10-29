0b2560b7ad42af9586ee078e1d1cffe589b7f730
Nourallah Mohammad Refaat Mohammad Abdelaal <nourallahm2004@gmail.com>
2023-10-29 17:40:05 +0100

    Initialising the BookController class

---
+++ src/main/java/com/example/onlinebookstore/BookController.java
@@ -0,0 +1,34 @@
+package com.example.onlinebookstore;
+
+import org.bookhaven.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addBook(@RequestBody Book book) {
+        if (bookRepository.findByTitle(book.getTitle()).isEmpty()) {
+
+            bookRepository.save(book);
+            return ResponseEntity.ok("Book added successfully");
+        } else {
+
+            return ResponseEntity.badRequest().body("A book with the same title already exists");
+        }
+    }
+
+}
