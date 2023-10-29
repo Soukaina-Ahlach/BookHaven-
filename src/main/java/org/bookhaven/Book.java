10cf5220dddfdceb55e10fdd64bc8fefd59f91eb
Nourallah Mohammad Refaat Mohammad Abdelaal <nourallahm2004@gmail.com>
2023-10-29 17:41:59 +0100

    creating the books.html

---
+++ src/main/java/org/bookhaven/Book.java
@@ -0,0 +1,38 @@
+package org.bookhaven;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Table(name = "books")
+@Entity
+public class Book {
+
+        @Getter
+        @Setter
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Getter
+        @Setter
+        @Column(unique = true)
+        @NotBlank
+        private String title;
+
+        @Getter
+        @Setter
+        @NotNull
+        private String author;
+
+        @Getter
+        @Setter
+        @Column(name = "publication_date")
+        private LocalDate publicationDate;
+
+
+}
