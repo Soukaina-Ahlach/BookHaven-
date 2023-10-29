3a388ed2c4e505b343ca0360665f4761f62af630
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-10-29 11:29:24 +0100

    Updating the OnlineBookStoreApplication.java class

--- src/main/java/com/example/onlinebookstore/OnlineBookstoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookstoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 
 @EntityScan(basePackages = "org.bookhaven")
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class OnlineBookstoreApplication {
 
 	public static void main(String[] args) {
