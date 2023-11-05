f6c235dc01718e7d39040060dee407efb0571be2
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-11-05 20:21:44 +0100

    Updating the BookRepository class

--- src/main/java/com/example/onlinebookstore/BookRepository.java
+++ src/main/java/com/example/onlinebookstore/BookRepository.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByTitle(String title);
 
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByGenre(String genre);
+
 }
