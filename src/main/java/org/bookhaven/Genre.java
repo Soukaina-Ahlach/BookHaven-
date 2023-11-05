395066068cc185fa68be6c40867dd61d68325c24
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-11-05 20:54:14 +0100

    Creating the Genre class

---
+++ src/main/java/org/bookhaven/Genre.java
@@ -0,0 +1,15 @@
+package org.bookhaven;
+
+public enum Genre {
+    ROMANCE,
+    ADVENTURE,
+    THRILLER,
+    COMEDY,
+    MYSTERY,
+    FANTASY,
+    BIOGRAPHY,
+    HISTORY,
+    COOKING,
+    TRAVEL,
+    SCIENCE
+}
