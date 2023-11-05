9b2de3b480d30341eb020e95f6cb1b722155192a
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-11-05 20:24:00 +0100

    Creating the GoogleBook class

---
+++ src/main/java/org/bookhaven/GoogleBook.java
@@ -0,0 +1,29 @@
+package org.bookhaven;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class GoogleBook {
+
+    @Getter
+    @Setter
+    private String title;
+
+    @Getter
+    @Setter
+    private String author;
+
+    @Getter
+    @Setter
+    private String coverImageUrl;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private String genre;
+
+}
+
