cbc8086721b1e7af68dd5a337dfdced8283648ac
Nourallah Mohammad Refaat Mohammad Abdelaal <nourallahm2004@gmail.com>
2023-11-10 19:07:06 +0100

    Implementing the searchBooksByQuery function

--- src/main/java/com/example/onlinebookstore/GoogleBooksService.java
+++ src/main/java/com/example/onlinebookstore/GoogleBooksService.java
@@ -86,5 +86,39 @@ public class GoogleBooksService {
 
         return null; // Return null if book not found
     }
+    public List<GoogleBook> searchBooksByQuery(String query) {
+        try {
+            String apiUrl = API_BASE_URL + "?q=" + query + "&maxResults=4";
+            ResponseEntity<GoogleBooksApiResponse> responseEntity = restTemplate.exchange(
+                    apiUrl, HttpMethod.GET, null, GoogleBooksApiResponse.class);
+
+            if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                GoogleBooksApiResponse apiResponse = responseEntity.getBody();
+                List<GoogleBook> books = new ArrayList<>();
+
+                if (apiResponse != null && apiResponse.getItems() != null) {
+                    for (GoogleBooksApiResponse.Item item : apiResponse.getItems()) {
+                        GoogleBook book = new GoogleBook();
+                        book.setTitle(item.getVolumeInfo().getTitle());
+                        book.setAuthor(String.join(", ", item.getVolumeInfo().getAuthors()));
+                        book.setCoverImageUrl(item.getVolumeInfo().getImageLinks().getThumbnail());
+                        book.setDescription(item.getVolumeInfo().getDescription());
+                        book.setGenre(String.join(", ", item.getVolumeInfo().getCategories()));
+
+                        double price = bookPricing.generateRandomPrice();
+                        book.setPrice(price);
+
+                        books.add(book);
+                    }
+                }
+                return books;
+            }
+        } catch (Exception e) {
+            // Handle exception
+        }
+
+        return Collections.emptyList();
+    }
+
 
 }
