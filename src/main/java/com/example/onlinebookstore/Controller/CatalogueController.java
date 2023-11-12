299e9495a2e36d05297c6661f8f898c4470af38d
ambermeng <marothimeng@gmail.com>
2023-11-12 14:11:05 +0100

    update getmapping catalogue and adding getmapping cataogue/{genre}

--- src/main/java/com/example/onlinebookstore/Controller/CatalogueController.java
+++ src/main/java/com/example/onlinebookstore/Controller/CatalogueController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 import java.util.List;
@@ -21,20 +22,20 @@ public class CatalogueController {
 
     @GetMapping("/catalogue")
     public String catalogue(Model model) {
+//
+        List<GoogleBook> romanceBooks = googleBooksService.searchBooksByGenre("romance",4);
+        List<GoogleBook> adventureBooks = googleBooksService.searchBooksByGenre("adventure",4);
+        List<GoogleBook> fictionBooks = googleBooksService.searchBooksByGenre("fiction",4);
+        List<GoogleBook> comedyBooks = googleBooksService.searchBooksByGenre("comedy",4);
+        List<GoogleBook> mysteryBooks = googleBooksService.searchBooksByGenre("mystery",4);
+        List<GoogleBook> fantasyBooks = googleBooksService.searchBooksByGenre("fantasy",4);
+        List<GoogleBook> biographyBooks = googleBooksService.searchBooksByGenre("biography",4);
+        List<GoogleBook> historyBooks = googleBooksService.searchBooksByGenre("history",4);
+        List<GoogleBook> cookingBooks = googleBooksService.searchBooksByGenre("cooking",4);
+        List<GoogleBook> travelBooks = googleBooksService.searchBooksByGenre("travel",4);
+        List<GoogleBook> scienceBooks = googleBooksService.searchBooksByGenre("science",4);
 
-        List<GoogleBook> romanceBooks = googleBooksService.searchBooksByGenre("romance");
-        List<GoogleBook> adventureBooks = googleBooksService.searchBooksByGenre("adventure");
-        List<GoogleBook> fictionBooks = googleBooksService.searchBooksByGenre("fiction");
-        List<GoogleBook> comedyBooks = googleBooksService.searchBooksByGenre("comedy");
-        List<GoogleBook> mysteryBooks = googleBooksService.searchBooksByGenre("mystery");
-        List<GoogleBook> fantasyBooks = googleBooksService.searchBooksByGenre("fantasy");
-        List<GoogleBook> biographyBooks = googleBooksService.searchBooksByGenre("biography");
-        List<GoogleBook> historyBooks = googleBooksService.searchBooksByGenre("history");
-        List<GoogleBook> cookingBooks = googleBooksService.searchBooksByGenre("cooking");
-        List<GoogleBook> travelBooks = googleBooksService.searchBooksByGenre("travel");
-        List<GoogleBook> scienceBooks = googleBooksService.searchBooksByGenre("science");
-
-
+        //
         model.addAttribute("romanceBooks", romanceBooks);
         model.addAttribute("adventureBooks", adventureBooks);
         model.addAttribute("fictionBooks", fictionBooks);
@@ -49,6 +50,18 @@ public class CatalogueController {
 
         return "catalogue";
     }
+
+    //
+    @GetMapping("/catalogue/{genre}")
+    public String genreCatalogue(Model model, @PathVariable String genre) {
+        List<GoogleBook> genreBooks = googleBooksService.searchBooksByGenre(genre,40);
+
+        System.out.println("Number of books in genreBooks: " + genreBooks.size());
+
+        model.addAttribute("genreBooks", genreBooks);
+        return "genres/" + genre;  // Assumes your genre pages are in a "genres" folder
+    }
+    //
 
     @GetMapping("/description")
     public String bookDescription(Model model, @RequestParam String title, @RequestParam String author) {
