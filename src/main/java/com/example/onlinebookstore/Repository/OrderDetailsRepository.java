dd85fe7bbdfb3f4d647d9135368c796a2c5497dd
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-11-19 18:24:55 +0100

    Creating the OrderDetailsRepository class

---
+++ src/main/java/com/example/onlinebookstore/Repository/OrderDetailsRepository.java
@@ -0,0 +1,13 @@
+package com.example.onlinebookstore.Repository;
+
+import org.bookhaven.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, String>{
+
+    OrderDetails findByOrderNumber(String orderNumber);
+}
