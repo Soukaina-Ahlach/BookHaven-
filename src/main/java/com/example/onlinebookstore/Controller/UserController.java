270a59f4a4a2f6f5fc6e290a7e548949873ac2e1
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-11-19 18:25:54 +0100

    Updating the UserController class

--- src/main/java/com/example/onlinebookstore/Controller/UserController.java
+++ src/main/java/com/example/onlinebookstore/Controller/UserController.java
@@ -1,18 +1,30 @@
 package com.example.onlinebookstore.Controller;
 
+import com.example.onlinebookstore.Repository.OrderDetailsRepository;
 import com.example.onlinebookstore.Repository.UserRepository;
 
+import org.bookhaven.OrderDetails;
 import org.bookhaven.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/")
@@ -20,50 +32,25 @@ public class UserController {
 
     @Autowired
     private final UserRepository userRepository;
+
+    @Autowired
+    private final OrderDetailsRepository orderDetailsRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private static final Set<String> allowedUsers = new HashSet<>();
+
+    static{
+        allowedUsers.add("shukrah");
+        allowedUsers.add("nour");
+        allowedUsers.add("mia");
+        allowedUsers.add("amber");
+        allowedUsers.add("soukaina");
+    }
+
+    public UserController(UserRepository userRepository, OrderDetailsRepository orderDetailsRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.orderDetailsRepository = orderDetailsRepository;
         this.passwordEncoder = passwordEncoder;
-    }
-
-    @GetMapping("/")
-    public String welcome() {
-        return "welcomepage";
-    }
-
-    @GetMapping("/index")
-    public String userIndex() {
-        return "index";
-    }
-
-    @GetMapping("/index/my-profile")
-    public String userProfile() {
-        return "profile-info";
-    }
-
-    @GetMapping("/index/my-orders")
-    public String userOrders() {
-        return "orders";
-    }
-
-    @GetMapping("/index/my-wishlist")
-    public String userWishList() {
-        return "wishlist";
-    }
-
-    @GetMapping("/index/settings")
-    public String userSettings() {
-        return "settings";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate(); // Invalidate the session
-        }
-        return "redirect:/"; // Redirect to the welcome page or any other desired page
     }
 
     @GetMapping("/css/{fileName:.+}")
@@ -89,6 +76,8 @@ public class UserController {
                 .header(HttpHeaders.CONTENT_TYPE, "text/javascript")
                 .body(resource);
     }
+
+    /* REGISTRATION,LOGIN AND AUTHORISATION */
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(
@@ -135,6 +124,126 @@ public class UserController {
         session.setAttribute("username", username);
 
         return ResponseEntity.ok("User logged in successfully");
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // Invalidate the session
+        }
+        return "redirect:/"; // Redirect to the welcome page or any other desired page
+    }
+
+    @GetMapping("/unauthorised")
+    public String unauthorised() {
+        return "unauthorised";
+    }
+
+    /* INDEX PAGES */
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcomepage";
+    }
+
+    @GetMapping("/index")
+    public String userIndex() {
+        return "index";
+    }
+
+    @GetMapping("/index/my-profile")
+    public String userProfile() {
+        return "profile-info";
+    }
+
+    @GetMapping("/index/my-orders")
+    public String userOrders() {
+        return "orders";
+    }
+
+    @GetMapping("/index/my-wishlist")
+    public String userWishList() {
+        return "wishlist";
+    }
+
+    @GetMapping("/index/settings")
+    public String userSettings() {
+        return "settings";
+    }
+
+    /* ORDER-MANAGEMENT */
+
+    @GetMapping("/order-management")
+    public String orderManagement(Model model, HttpSession session) {
+        Boolean loggedIn = (Boolean) session.getAttribute("loggedIn");
+        String username = (String) session.getAttribute("username");
+
+        if (loggedIn != null && loggedIn && allowedUsers.contains(username)) {
+            List<OrderDetails> orderList = orderDetailsRepository.findAll();
+            model.addAttribute("orderList", orderList);
+            System.out.println("SHOWING ORDER MANAGEMENT PAGE");
+            return "order-management";
+        } else {
+            return "redirect:/unauthorised";
+        }
+    }
+
+    @GetMapping("/getOrders")
+    @ResponseBody
+    public List<OrderDetails> getOrders() {
+        List<OrderDetails> orderDetailsList = orderDetailsRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+        orderDetailsList.forEach(orderDetails -> orderDetails.setStatus(orderDetails.getStatus()));
+
+        return orderDetailsList;
+    }
+
+    @GetMapping("/getOrderDetails")
+    @ResponseBody
+    public ResponseEntity<OrderDetails> getOrderDetails(@RequestParam String orderNumber) {
+        try {
+            // Retrieve order details by order number
+            OrderDetails orderDetails = orderDetailsRepository.findByOrderNumber(orderNumber);
+            return ResponseEntity.ok(orderDetails);
+        } catch (Exception e) {
+            // Handle exceptions appropriately (e.g., log the error)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PostMapping( "/saveOrder")
+    @ResponseBody
+    public ResponseEntity<String> saveOrderDetails(@RequestBody OrderDetails orderDetails, HttpSession session) {
+
+        try {
+            String username = (String) session.getAttribute("username");
+            User user = userRepository.findByUsername(username);
+
+            orderDetails.setUsername(username);
+            orderDetails.setEmail(user.getEmail());
+            orderDetails.setPhoneNumber(user.getPhoneNumber());
+            orderDetails.setCreatedAt(LocalDateTime.now());
+            // You can use the OrderDetailsRepository to save the order details
+            orderDetailsRepository.save(orderDetails);
+            return ResponseEntity.ok("Order details saved successfully");
+        } catch (Exception e) {
+            // Handle exceptions appropriately (e.g., log the error)
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error saving order details");
+        }
+
+    }
+
+    @PutMapping("/updateOrderStatus")
+    public ResponseEntity<String> updateOrderStatus(@RequestParam String orderNumber, @RequestParam String status) {
+
+        OrderDetails order = orderDetailsRepository.findByOrderNumber(orderNumber);
+        if (order != null) {
+            order.setStatus(status);
+            orderDetailsRepository.save(order);
+            return ResponseEntity.ok("Order status updated successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Order not found");
+        }
     }
 
     // Other methods
