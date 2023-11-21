ceecbffe6bff8107e25fc32a049c23a8e6d38136
Shukrah Adekumbi Ozohu Alimi <shukrah-036@mailbox.unideb.hu>
2023-11-21 22:54:30 +0100

    Adding Javadoc documentation for the Genre class

--- src/main/java/org/bookhaven/Genre.java
+++ src/main/java/org/bookhaven/Genre.java
@@ -1,5 +1,13 @@
 package org.bookhaven;
 
+/**
+ * The {@code Genre} enum represents different genres that can be assigned to books.
+ * It includes predefined genres such as ROMANCE, ADVENTURE, THRILLER, COMEDY, MYSTERY,
+ * FANTASY, BIOGRAPHY, HISTORY, COOKING, TRAVEL, and SCIENCE.
+ * <p>
+ * This enum is used to categorize books based on their genre.
+ *
+ */
 public enum Genre {
     ROMANCE,
     ADVENTURE,
